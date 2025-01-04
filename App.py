ea4ad7cc829bf41c89d3f19b8c953dd29ccd1478
shafiranabila041031 <shafira.23011@mhs.unesa.ac.id>
2025-01-05 04:45:38 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,7 +1,5 @@
 import streamlit as st
-# import pandas as pd
 import base64
-# import joblib
 import numpy as np
 from joblib import load
 
@@ -21,13 +19,6 @@ def predict_tree(tree, x):
         else:
             tree = tree['right_child']
     return tree['prediction']
-
-# # Load the trained model
-# model = joblib.load('randomforest_model.joblib')
-
-# # Load Dataset
-# file_path = 'alzheimers_data.csv'
-# dataset = pd.read_csv(file_path)
 
 # GUI Streamlit
 st.set_page_config(page_title="Cognify", page_icon="🧠", layout="centered")
@@ -80,34 +71,52 @@ def prev_page():
 if st.session_state.page == 1:
     add_background("Home Cognify.png")
 
-    st.write("") 
-    st.write("")  
-    st.write("")  
-    st.write("")  
-    st.write("")  
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
+    # Tambahkan spacing vertikal seperti sebelumnya
+    for _ in range(27):  # Menggantikan multiple st.write("")
+        st.write("")
 
+    st.markdown("""
+        <style>
+        .stButton>button {
+            background-color: grey;
+            color: #0C2227;
+            border: 1px solid grey;
+            margin-left: 100px;  /* Tambahkan margin dari kiri */
+            position: relative;  /* Tambahkan positioning */
+            left: -200px;      /* Geser ke kiri */
+        }
+        .stButton>button:hover {
+            background-color: #64D98A;
+            color: #0C2227;
+            border: #64D98A;
+        }
+        .stButton>button:active {
+            background-color: #64D98A !important;
+            color: #0C2227 !important;
+            border: #64D98A;
+        }
+        .stButton>button:focus {
+            background-color: #64D98A !important;
+            color: #0C2227 !important;
+            border: #64D98A !important;
+            box-shadow: none !important;
+        }
+        .stButton>button:focus:not(:focus-visible) {
+            outline: none !important;
+        }
+        
+        .block-container {
+        margin-left: 50px;   /* Lebih ke kiri */
+        left: -100px;        /* Lebih ke kiri lagi */
+        }
+        
+        div[data-testid="stVerticalBlock"] {
+        margin-left: 50px;   /* Lebih ke kiri */
+        left: -100px;        /* Lebih ke kiri lagi */
+        }
+        </style>
+        """, unsafe_allow_html=True)
+    
     if st.button("Start Your Detection Now"):
         next_page()
 
@@ -115,54 +124,112 @@ if st.session_state.page == 1:
 elif st.session_state.page == 2:
     add_background("Detection Cognify.png") 
 
-    st.write("") 
-    st.write("")  
-    st.write("")  
-    st.write("")  
-    st.write("")  
-    st.write("")  
-    st.write("")  
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
+    for _ in range(10): 
+        st.write("")
+    
+    st.markdown("""
+    <style>
+    /* Style untuk number input container */
+    .stNumberInput div[data-baseweb="input"] {
+                # background-color: #1E1E1E !important;
+        border: none !important;
+    }
+                
+    /* Style untuk tombol plus dan minus saat hover */
+    .stNumberInput button:hover {
+        background-color: #64D98A !important;
+        color: white !important;
+    }
+    
+    /* Style untuk tombol plus dan minus saat active/diklik */
+    .stNumberInput button:active {
+        background-color: #64D98A !important;
+        color: white !important;
+    }
+    
+    /* Style untuk number input field */
+    input[type="number"] {
+                # background-color: #1E1E1E !important;
+        color: white !important;
+        border: none !important;
+    }
 
+    /* Menghilangkan border focus */
+    .stNumberInput div[data-baseweb="input"]:focus-within {
+        border: none !important;
+        box-shadow: none !important;
+    }
+    
+    .stButton>button:focus {
+    background-color: #64D98A !important;
+    color: #0C2227 !important;
+    border: #64D98A !important;
+    box-shadow: none !important;
+    }
+                
+    /* Mencegah outline merah saat focus */
+    .stButton>button:focus:not(:focus-visible) {
+        outline: none !important;
+    }
+    </style>
+""", unsafe_allow_html=True)
+    
     # Gunakan kolom untuk tata letak text input
     col1, col2 = st.columns(2)
 
     with col1:
         memory_complaints = st.number_input(
             "Memory Complaints (0-10):",
-            min_value=0.0, max_value=10.0, value=5.0, step=1.0,
-            help="A test that measures a person's thinking and remembering abilities, with a score of 0-10 indicating serious problems, 11-20 moderate problems, and 21-30 normal conditions."
+            min_value=0.0, max_value=10.0, value=0.0, step=1.0,
+            help="A measurement of how much a person feels they have problems with their memory, where 0 means no problems and 10 means very serious problems."
         )
         behavioral_problems = st.number_input(
             "Behavioral Problems (0-10):",
-            min_value=0.0, max_value=10.0, value=5.0, step=1.0,
-            help="Level of behavioral issues"
+            min_value=0.0, max_value=10.0, value=0.0, step=1.0,
+            help="An assessment of a person's behavioral changes or emotional problems, with 0 signifying no problem and 10 signifying very difficult behavior such as anger or confusion."
         )
         mmse = st.number_input(
             "MMSE Score (0-30):",
-            min_value=0.0, max_value=30.0, value=15.0, step=1.0,
-            help="Cognitive function test"
+            min_value=0.0, max_value=30.0, value=0.0, step=1.0,
+            help="A measurement of how much a person feels they have problems with their memory, where 0 means no problems and 10 means very serious problems."
         )
 
     with col2:
         adl = st.number_input(
             "ADL Score (0-10):",
-            min_value=0.0, max_value=10.0, value=5.0, step=1.0,
-            help="Basic daily activities"
+            min_value=0.0, max_value=10.0, value=0.0, step=1.0,
+            help="An assessment of the ability to perform daily activities such as eating, bathing, and dressing, with 0 indicating the ability to do everything by oneself and 10 indicating the inability to do anything by oneself."
         )
         functional_assessment = st.number_input(
             "Functional Assessment Score (0-10):",
-            min_value=0.0, max_value=10.0, value=5.0, step=1.0,
-            help="Ability to perform daily functions"
+            min_value=0.0, max_value=10.0, value=0.0, step=1.0,
+            help="An evaluation of a person's ability to care for themselves, where 0 means fully independent and 10 means needing a lot of help."
         )
 
     # Load model dari file
     model_path = 'randomforest_model.joblib'
     forest = load(model_path)
 
+    st.markdown("""
+        <style>
+        .stButton>button {
+            background-color: grey;
+            color: #0C2227;
+            border: 1px solid grey;
+        }
+        .stButton>button:hover {
+            background-color: #64D98A;
+            color: #0C2227;
+            border: #64D98A;
+        }
+        .stButton>button:active {
+            background-color: #64D98A !important;
+            color: #0C2227 !important;
+            border: #64D98A;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+        
     # Prediksi jika tombol ditekan
     if st.button("Prediksi"):
         # Data input dari user
