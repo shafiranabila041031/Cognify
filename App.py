12930fe71d1d96418bceeb1694f7fc2e64584f8f
shafiranabila041031 <shafira.23011@mhs.unesa.ac.id>
2025-01-05 18:49:36 +0700

    Update App.py

--- App.py
+++ App.py
@@ -52,14 +52,6 @@ def add_background(image_file):
     header, footer {{
         background-color: rgba(0, 0, 0, 0) !important;
     }}
-    @media (max-width: 600px) {{
-        .stButton>button {{
-            width: 80%;  /* Lebar tombol lebih besar di layar kecil */
-        }}
-        .block-container {{
-            margin-left: 0;  /* Menghilangkan margin di layar kecil */
-        }}
-    }}
     </style>
     """
     st.markdown(css, unsafe_allow_html=True)
@@ -79,7 +71,9 @@ def prev_page():
 if st.session_state.page == 1:
     add_background("Home Cognify.png")
 
-    st.markdown("<div style='height: 20vh;'></div>", unsafe_allow_html=True)  # Spacing vertikal
+    # Tambahkan spacing vertikal seperti sebelumnya
+    for _ in range(27):  # Menggantikan multiple st.write("")
+        st.write("")
 
     st.markdown("""
         <style>
@@ -87,14 +81,38 @@ if st.session_state.page == 1:
             background-color: grey;
             color: #0C2227;
             border: 1px solid grey;
-            width: 40%;  /* Menggunakan persentase untuk lebar */
-            margin: 0 auto;  /* Center button */
-            display: block;  /* Center button */
+            margin-left: 100px;  /* Tambahkan margin dari kiri */
+            position: relative;  /* Tambahkan positioning */
+            left: -200px;      /* Geser ke kiri */
         }
         .stButton>button:hover {
             background-color: #64D98A;
             color: #0C2227;
             border: #64D98A;
+        }
+        .stButton>button:active {
+            background-color: #64D98A !important;
+            color: #0C2227 !important;
+            border: #64D98A;
+        }
+        .stButton>button:focus {
+            background-color: #64D98A !important;
+            color: #0C2227 !important;
+            border: #64D98A !important;
+            box-shadow: none !important;
+        }
+        .stButton>button:focus:not(:focus-visible) {
+            outline: none !important;
+        }
+        
+        .block-container {
+        margin-left: 70px;   /* Lebih ke kiri */
+        left: -150px;        /* Lebih ke kiri lagi */
+        }
+        
+        div[data-testid="stVerticalBlock"] {
+        margin-left: 70px;   /* Lebih ke kiri */
+        left: -150px;        /* Lebih ke kiri lagi */
         }
         </style>
         """, unsafe_allow_html=True)
@@ -106,19 +124,87 @@ if st.session_state.page == 1:
 elif st.session_state.page == 2:
     add_background("Detection Cognify.png") 
 
-    st.markdown("<div style='height: 10vh;'></div>", unsafe_allow_html=True)  # Spacing vertikal
+    for _ in range(10): 
+        st.write("")
+    
+    st.markdown("""
+    <style>
+    /* Style untuk number input container */
+    .stNumberInput div[data-baseweb="input"] {
+                # background-color: #1E1E1E !important;
+        border: none !important;
+    }
+                
+    /* Style untuk tombol plus dan minus saat hover */
+    .stNumberInput button:hover {
+        background-color: #64D98A !important;
+        color: white !important;
+    }
+    
+    /* Style untuk tombol plus dan minus saat active/diklik */
+    .stNumberInput button:active {
+        background-color: #64D98A !important;
+        color: white !important;
+    }
+    
+    /* Style untuk number input field */
+    input[type="number"] {
+                # background-color: #1E1E1E !important;
+        color: white !important;
+        border: none !important;
+    }
+
+    /* Menghilangkan border focus */
+    .stNumberInput div[data-baseweb="input"]:focus-within {
+        border: none !important;
+        box-shadow: none !important;
+    }
+    
+    .stButton>button:focus {
+    background-color: #64D98A !important;
+    color: #0C2227 !important;
+    border: #64D98A !important;
+    box-shadow: none !important;
+    }
+                
+    /* Mencegah outline merah saat focus */
+    .stButton>button:focus:not(:focus-visible) {
+        outline: none !important;
+    }
+    </style>
+""", unsafe_allow_html=True)
     
     # Gunakan kolom untuk tata letak text input
     col1, col2 = st.columns(2)
 
     with col1:
-        memory_complaints = st.number_input("Memory Complaints (0-10):", min_value=0.0, max_value=10.0, value=0.0, step=1.0)
-        behavioral_problems = st.number_input("Behavioral Problems (0-10):", min_value=0.0, max_value=10.0, value=0.0, step=1.0)
-        mmse = st.number_input("MMSE Score (0-30):", min_value=0.0, max_value=30.0, value=0.0, step=1.0)
+        memory_complaints = st.number_input(
+            "Memory Complaints (0-10):",
+            min_value=0.0, max_value=10.0, value=0.0, step=1.0,
+            help="A measurement of how much a person feels they have problems with their memory, where 0 means no problems and 10 means very serious problems."
+        )
+        behavioral_problems = st.number_input(
+            "Behavioral Problems (0-10):",
+            min_value=0.0, max_value=10.0, value=0.0, step=1.0,
+            help="An assessment of a person's behavioral changes or emotional problems, with 0 signifying no problem and 10 signifying very difficult behavior such as anger or confusion."
+        )
+        mmse = st.number_input(
+            "MMSE Score (0-30):",
+            min_value=0.0, max_value=30.0, value=0.0, step=1.0,
+            help="A measurement of how much a person feels they have problems with their memory, where 0 means no problems and 10 means very serious problems."
+        )
 
     with col2:
-                adl = st.number_input("ADL Score (0-10):", min_value=0.0, max_value=10.0, value=0.0, step=1.0)
-                functional_assessment = st.number_input("Functional Assessment Score (0-10):", min_value=0.0, max_value=10.0, value=0.0, step=1.0)
+        adl = st.number_input(
+            "ADL Score (0-10):",
+            min_value=0.0, max_value=10.0, value=0.0, step=1.0,
+            help="An assessment of the ability to perform daily activities such as eating, bathing, and dressing, with 0 indicating the ability to do everything by oneself and 10 indicating the inability to do anything by oneself."
+        )
+        functional_assessment = st.number_input(
+            "Functional Assessment Score (0-10):",
+            min_value=0.0, max_value=10.0, value=0.0, step=1.0,
+            help="An evaluation of a person's ability to care for themselves, where 0 means fully independent and 10 means needing a lot of help."
+        )
 
     # Load model dari file
     model_path = 'randomforest_model.joblib'
@@ -130,13 +216,15 @@ elif st.session_state.page == 2:
             background-color: grey;
             color: #0C2227;
             border: 1px solid grey;
-            width: 40%;  /* Menggunakan persentase untuk lebar */
-            margin: 0 auto;  /* Center button */
-            display: block;  /* Center button */
         }
         .stButton>button:hover {
             background-color: #64D98A;
             color: #0C2227;
+            border: #64D98A;
+        }
+        .stButton>button:active {
+            background-color: #64D98A !important;
+            color: #0C2227 !important;
             border: #64D98A;
         }
         </style>
@@ -167,12 +255,6 @@ elif st.session_state.page == 3:
     # Positive prediction (detected Alzheimer's)
     if st.session_state.positive == 1:
         add_background("Result Positive.png")
-        st.markdown("<h1 style='text-align: center; color: white;'>Terdeteksi Alzheimer</h1>", unsafe_allow_html=True)
     else:
         # Negative prediction (not detected)
         add_background("Result Negative.png")
-        st.markdown("<h1 style='text-align: center; color: white;'>Tidak Terdeteksi Alzheimer</h1>", unsafe_allow_html=True)
-
-    # Tombol untuk kembali ke halaman utama
-    if st.button("Kembali ke Halaman Utama"):
-        st.session_state.page = 1
